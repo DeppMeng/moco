b8d019ccef66d109b25a8f0d07e0e48b3178f700
DeppMeng <depp_meng@bitbucket.org>
2020-03-23 00:38:51 +0800

    debugging zip loader

--- scripts/train_eval_imagenet_default_debug.sh
+++ scripts/train_eval_imagenet_default_debug.sh
@@ -3,7 +3,7 @@
 data_dir="./data/imagenet/images"
 output_dir="./output/imagenet/K65536_default_exp1"
 python -m torch.distributed.launch --nproc_per_node=4 \
-    train.py \
+    train_debug.py \
     --data-dir ${data_dir} \
     --dataset imagenet \
     --nce-k 65536 \
